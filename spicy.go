01583e73a26352e143d119e8b8849f54532e19e3
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2018-02-24 14:58:00 -0800

    Move to a much smaller (and more reasonable?) definition.

--- spicy.go
+++ spicy.go
@@ -8,64 +8,73 @@ import (
   "github.com/alecthomas/participle"
 )
 
-type Value struct {
-  Symbol string `@Ident |`
-  Constant int `@Int`
-}
-
-type StackValue struct {
-  Value Value `@@`
-  AddedValues []*Value `{"+" @@}`
+type Constant struct {
+  Symbol string `  @Ident`
+  Int int       `| @Int`
 }
 
 type Flag struct {
-  Boot bool `@"BOOT" |`
-  Object bool `@"OBJECT" |`
-  Raw bool `@"RAW"`
+  Boot bool   `  @"BOOT"`
+  Object bool `| @"OBJECT"`
+  Raw bool    `| @"RAW"`
 }
 
-type SegmentStatement struct {
+type Summand struct {
+   Lhs *Constant ` @@`
+   Op  string    `[ @("+" | "-")`
+   Rhs *Constant ` @@ ]`
+ }
+
+type MaxSegment struct {
+  First string `"max[" @String ","`
+  Second string `@String "]"`
+}
+
+type MinSegment struct {
+  First string `"min[" @String ","`
+  Second string `@String "]"`
+}
+
+// Only one of these values will be set.
+type Value struct {
+  String string    `  @String`
+  Flags []*Flag    `| @@ { @@ }`
+  ConstantValue *Summand `| @@`
+  MaxSegment *MaxSegment `| @@`
+  MinSegment *MinSegment `| @@`
+}
+
+type Statement struct {
 /*
  :name <segmentName>
-                |address <constant>
-                |after <segmentName>
-                |after max[<segmentName>,<segmentName>]
-                |after min[<segmentName>,<segmentName>]
-                |include <filename>
-                |maxsize <constant>
-                |align <constant>
-                |flags <flagList>
-                |number <constant>
-                |entry <symbol>
-                |stack <stackValue>
+ |address <constant>
+ |after <segmentName>
+ |after max[<segmentName>,<segmentName>]
+ |after min[<segmentName>,<segmentName>]
+ |include <filename>
+ |maxsize <constant>
+ |align <constant>
+ |flags <flagList>
+ |number <constant>
+ |entry <symbol>
+ |stack <stackValue>
 */
-  Name string      `"name" @String |`
-  Address     int `"address" @Int |`
-  After       string      `"after" @String |`
-  Include     string     `"include" @String |`
-  MaxSize     string     `"maxsize" @String |`
-  Align       string     `"align" @String |`
-  Flags       []*Flag `"flags" { @@ } |`
-  Number      int `"number" @Int|`
-  Entry string     `"entry" @Ident |`
-  Stack StackValue `"stack" @@`
+  // I tried using @Ident here, but the parser was greedily taking 'endseg' as name.
+  // By explicitly listing all known names here, we limit the search space.
+  Name string `@("name" | "address" | "after" | "include" | "maxsize" | "align" | "flags" | "number" | "entry" | "stack")`
+  Value Value `@@`
+}
+
+type Statements struct {
+  Statements []*Statement `{ @@ }`
 }
 
 type Segment struct {
-  SegmentStatementList []*SegmentStatement `"beginseg" { @@ } "endseg"`
-}
-
-type WaveStatement struct {
-/*
- :name <waveName>
-                |include <segmentName>
-*/
-  Name string      `"name" @String |`
-  Include     string     `"include" @String`
+  StatementList Statements `"beginseg" @@ "endseg"`
 }
 
 type Wave struct {
-  WaveStatementList []*WaveStatement `"beginwave" { @@ } "endwave"`
+  StatementList Statements `"beginwave" @@ "endwave"`
 }
 
 type Spec struct {
