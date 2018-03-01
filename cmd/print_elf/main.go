e8fd0848422800dcde1298924e33bc4c77f33b3b
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2018-02-28 16:45:19 -0800

    Run gofmt.

--- cmd/print_elf/main.go
+++ cmd/print_elf/main.go
@@ -1,18 +1,21 @@
 package main
+
 import (
-  "debug/elf"
-  "os"
-  "fmt"
+	"debug/elf"
+	"fmt"
+	"os"
 )
 
 func main() {
-  path := os.Args[1]
-  f, err := elf.Open(path)
-  if err != nil { panic(err) }
-  defer f.Close()
+	path := os.Args[1]
+	f, err := elf.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
 
-  fmt.Printf("%+v\n", f)
-  for _, sec := range(f.Sections) {
-    fmt.Printf("%+v\n", sec.SectionHeader)
-  }
+	fmt.Printf("%+v\n", f)
+	for _, sec := range f.Sections {
+		fmt.Printf("%+v\n", sec.SectionHeader)
+	}
 }
