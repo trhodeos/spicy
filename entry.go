642b46710f94157373d99d8d0ab6d853eb7a12cb
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2018-03-20 07:36:47 -0700

    Pipe the entry source directly into the runner.
    This cleans up the code quite a bit.

--- entry.go
+++ entry.go
@@ -3,15 +3,14 @@ package spicy
 import (
 	"bytes"
 	log "github.com/sirupsen/logrus"
-	"io/ioutil"
+	"io"
 	"os"
-	"path/filepath"
 	"text/template"
 )
 
 var compileArgs = []string{"-march=vr4300", "-mtune=vr4300", "-mgp32", "-mfp32", "-non_shared"}
 
-func createEntrySource(bootSegment *Segment) (string, error) {
+func createEntrySource(bootSegment *Segment) (io.Reader, error) {
 	t := `
 	.text
 	.global	_start
@@ -27,48 +26,24 @@ _start:
 	la	$10, {{.Entry}} + 0
 	la	$29,{{.StackInfo.Start}} + {{.StackInfo.Offset}}
 	jr	$10
-
 `
 	tmpl, err := template.New("test").Parse(t)
 	if err != nil {
-		return "", err
+		return nil, err
 	}
 	b := &bytes.Buffer{}
 	err = tmpl.Execute(b, bootSegment)
-	return b.String(), err
-}
-
-func generateEntryScript(w *Wave) (string, error) {
-	log.Debug("Starting to generate entry script.")
-	content, err := createEntrySource(w.GetBootSegment())
-	if err != nil {
-		return "", err
-	}
-	log.Debugf("Entry script generated:\n%s", content)
-	tmpfile, err := ioutil.TempFile("", "entry-script")
-	path, err := filepath.Abs(tmpfile.Name())
-	if err != nil {
-		return "", err
-	}
-	log.Debug("Writing script to", path)
-	if _, err := tmpfile.Write([]byte(content)); err != nil {
-		return "", err
-	}
-	if err := tmpfile.Close(); err != nil {
-		return "", err
-	}
-	log.Debug("Script written.")
-	return path, nil
+	return b, err
 }
 
 func CreateEntryBinary(w *Wave, as Runner) (*os.File, error) {
 	name := w.Name
 	log.Infof("Creating entry for \"%s\".", name)
-	entrySourcePath, err := generateEntryScript(w)
+	entrySource, err := createEntrySource(w.GetBootSegment())
 	if err != nil {
 		return nil, err
 	}
-	_, err = as.Run( /* stdin=*/ nil, append(compileArgs, entrySourcePath))
+	_, err = as.Run(entrySource, append(compileArgs, "-"))
 	if err != nil {
 		return nil, err
 	}
