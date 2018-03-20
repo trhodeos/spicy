ceab73f980d5de4d1ddaece4aba69e99571ced82
Tyler Rhodes <tyler.s.rhodes@gmail.com>
2018-03-19 19:07:26 -0700

    Use testify instead of bare testing methods.

--- spec_test.go
+++ spec_test.go
@@ -1,11 +1,13 @@
 package spicy
 
 import (
+	"github.com/stretchr/testify/assert"
 	"strings"
 	"testing"
 )
 
 func TestParsingSimpleSpec(t *testing.T) {
+	assert := assert.New(t)
 	specStr := `
 beginseg
   name "some_segment"
@@ -16,11 +18,7 @@ beginwave
 endwave
 `
 	spec, err := ParseSpec(strings.NewReader(specStr))
-	if err != nil {
-		t.Errorf("Encountered unexpected error: %s", err)
-	}
-	if len(spec.Waves) != 1 {
-		t.Errorf("Expected 1 wave, found %d", len(spec.Waves))
-	}
+	assert.Nil(err)
+	assert.Equal(1, len(spec.Waves))
 
 }
